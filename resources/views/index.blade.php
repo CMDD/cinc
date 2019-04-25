179bd67505ce3f5699ab3d59f08f8092743d0511
John Alvaro <jhon54plex@hotmail.com>
2019-04-24 21:23:25 -0500

    Inicio de proyecto

---
+++ resources/views/index.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.web')
+@section('content')
+<section class="ftco-section-2">
+				<div class="photograhy">
+					<div class="row no-gutters">
+						<div class="col-md-4 ftco-animate">
+							<a href="img/IMG_8690.JPG" class="photography-entry img image-popup d-flex justify-content-center align-items-center" style="background-image: url(img/IMG_8690.JPG);">
+								<div class="overlay"></div>
+								<!-- <div class="text text-center">
+									<h3>Work 01</h3>
+									<span class="tag">Model</span>
+								</div> -->
+							</a>
+						</div>
+					
+					</div>
+				</div>
+			</section>
+@stop
