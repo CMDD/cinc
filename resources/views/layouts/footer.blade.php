102d29eca8f3cf809c21305b79204bafb77d4c29
Desarrollo <desarrollo@iMac-de-John.local>
2019-04-29 15:08:57 -0500

    Footer nuevo

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,38 +1,22 @@
 <footer class="ftco-footer ftco-bg-dark ftco-section">
-    <div class="container px-md-5">
-        <div class="row mb-5">
-            <div class="col-md">
-            <div class="ftco-footer-widget mb-4 ml-md-4">
-            <img src="img/logo_footer.png" alt="">
-            
-            </div>
-            </div>
-            <div class="col-md">
-            <div class="ftco-footer-widget mb-4">
-            <h2 class="ftco-heading-2">Archives</h2>
-            <ul class="list-unstyled categories">
-            <li><a href="#">November 2018 <span>(105)</span></a></li>
-            <li><a href="#">October 2018 <span>(212)</span></a></li>
-            <li><a href="#">September 2018 <span>(150)</span></a></li>
-            <li><a href="#">August 2018 <span>(100)</span></a></li>
-            <li><a href="#">July 2018 <span>(200)</span></a></li>
-            </ul>
-            </div>
-            </div>
-            <div class="col-md">
-            <div class="ftco-footer-widget mb-4">
-             <img class="cinco" src="img/cinco.png" alt="">
-            </div>
-            </div>
-            </div>
-        </div>
+    <div class="container-fluid">
         <div class="row">
-        <div class="col-md-12">
-
-        <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-        Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-        <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
-        </div>
+            <div class="col-12 footer-box">
+                <div class="logo">
+                    <img src="img/logo_footer.png" alt="">
+                    <div class="social"><a href="#"><img src="img/facebook.svg" width="50"></a><a href="#"><img src="img/instagram.svg" width="50"></a></div>
+                </div>
+                <div class="contact">
+                    <h3>Contact</h3>
+                    <span><img src="img/maps.svg" width="20">Find us on Google Maps</span>
+                    <span><img src="img/whatsapp.svg" width="20">+12345678901</span>
+                    <span><img src="img/mail.svg" width="20">info@domain.com</span>
+                    <img src="img/3angles.svg" class="angles">
+                </div>
+                <div class="five">
+                    <img src="img/cinco.png">
+                </div>
+            </div>
         </div>
     </div>
 </footer>
