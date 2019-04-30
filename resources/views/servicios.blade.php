be34a175036cac8b88c5330c826906624bc4cff3
Desarrollo <desarrollo@iMac-de-John.local>
2019-04-30 15:41:34 -0500

    Cajas en servicios

--- resources/views/servicios.blade.php
+++ resources/views/servicios.blade.php
@@ -1,16 +1,36 @@
 @extends('layouts.web')
 @section('content')
-<section class="ftco-section bg-light ftco-bread">
-				<div class="container">
-					<div class="row no-gutters slider-text align-items-center">
-	          <div class="col-md-9 ftco-animate">
-	            <p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home</a></span> <span>Services</span></p>
-	            <h1 class="mb-3 bread">Services</h1>
-	            <p>Specializing in the creation of exceptional events for private and corporate clients, we design, plan and manage every project from conception to execution <br><br>
 
-	        </div>
-				</div>
-			</section>
-			
-			
+<section class="ftco-section bg-light ftco-bread">
+    <div class="container">
+        <div class="row no-gutters slider-text align-items-center">
+            <div class="col-md-9 ftco-animate">
+                <p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home</a></span> <span>Services</span></p>
+                <h1 class="mb-3 bread">Services</h1>
+                <p>Specializing in the creation of exceptional events for private and corporate clients, we design, plan and manage every project from conception to execution <br><br>
+                <div class="boxes">
+                    <div class="box-service">
+                        <span>ACCOMODATION SOURCING</span>
+                    </div>
+                    <div class="box-service">
+                        <span>ACCOMODATION</span>
+                    </div>
+                    <div class="box-service">
+                        <span>ACCOMODATION SOURCING</span>
+                    </div>
+                    <div class="box-service">
+                        <span>ACCOMODATION SOURCING</span>
+                    </div>
+                    <div class="box-service">
+                        <span>ACCOMODATION SOURCING</span>
+                    </div>
+                    <div class="box-service">
+                        <span>ACCOMODATION SOURCING</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
 @stop
